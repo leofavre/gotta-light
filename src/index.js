cddc28fbb7c9235dd5add7498156145363f568ee
Leo Favre <leonardo.favre@inbrands.com.br>
2017-07-20 12:04:04 -0300

    first stage reducer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import { createStore } from "redux";
+
+const stage = (state, action) => {
+	if (action.type === "RESIZE_STAGE") {
+		return {
+			width: action.width,
+			height: action.height
+		};
+	}
+	return state;
+};
